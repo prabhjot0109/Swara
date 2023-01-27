28cc7a56a5d7122b459604e9f0941f6b26f0d400
Swastik Bansal <swastikbansal0@gmail.com>
2023-01-27 20:57:40 +0530

    Improved Login Win GUI

--- Main.py
+++ Main.py
@@ -16,59 +16,69 @@ def fileWin():
 def loginWin():
     clearWin(win_root)
 
+    win_root.geometry("260x200")
+
+    #Login Win Header Frame
+    login_h_fr=Frame(win_root, bg="paleturquoise", borderwidth= 6, relief = GROOVE )
+    login_h_fr.pack(side=TOP, fill = X)
+
+    #Login Label
+    login_text=Label(login_h_fr, text="LOGIN" ,bg="white", fg = "red", font= ("Posterama  20 bold"), padx = 200)
+    login_text.pack()
+    
+
     #User
-    user = Label(win_root , text= "Username ID -  ")
-    user.place(x=50,y=50)
+    user = Label(win_root , text= "Username ID  :   " , bg="paleturquoise")
+    user.place(x= 30 , y = 58)
 
     userentry = Entry(win_root , textvariable =uservalue )
-    userentry.place(x=23, y = 28)
+    userentry.place(x=120, y = 58)
 
     #Password
-    password = Label(win_root , text= "Password - ")   
-    password.place(x=70,y =70)
+    password = Label(win_root , text= "Password       : ", bg="paleturquoise")  
+    password.place(x = 30,y=80)
 
-    passentry = Entry(win_root , textvariable =passvalue )
-    passentry.place(x=10,y=20)
+    passentry = Entry(win_root , textvariable = passvalue)
+    passentry.place(x=120 ,y = 80)
     
-    #Start Button
-    log_win = Button(win_root, fg="red", text = "START"  ,font = " arial 10 bold", bg= "white", command = fileWin)
-    log_win.pack(side = BOTTOM , padx = 20 , anchor = SE) 
+    #Login Button
+    log_b = Button(win_root, fg="red", text = "Login"  ,font = "raleway 12 bold", command = fileWin)
+    log_b.place(x = 170, y = 130)
+
+    #Create User Button
+    c_user_b = Button(win_root, fg = "red", text = "Sign - Up", font = " raleway 12 bold" )
+    c_user_b.place(x = 30, y = 130)
 
 #__main__
-
 #--------------- Main Window ---------------------
+#Window Properties
 win_root = Tk() 
+win_root.geometry("450x460")
+win_root.configure(bg="paleturquoise")
+win_root.resizable(False,False)
+win_root.title("Swara")
 
 #Global Variables
 uservalue = StringVar()
 passvalue = StringVar()
 
-
-#Geometry Of Win
-win_root.geometry("500x500")
-win_root.resizable(False,False)
-
-#Window Title
-win_root.title("Swara")
-
-#Frame in Tkinter
-wel_fr=Frame(win_root, bg="white", borderwidth= 6, relief = SUNKEN )
+#Welcome Text frame
+wel_fr=Frame(win_root, bg="white", borderwidth= 6, relief = GROOVE )
 wel_fr.pack(side=TOP, fill = X)
 
 #Welcome Label
-wel_text=Label(wel_fr, text="""Welcome to Swara\nA way to trace your progress""" ,
-                 bg="white", fg = "red", font= "times 20 bold", padx = 200)
+wel_text=Label(wel_fr, text="SWARA", fg = "red", font= ("Posterama  40 bold"), padx = 200)
 wel_text.pack()
 
 #Import image
 sw_photo = PhotoImage(file="Images/logo.png")
 sw_image = Label(image=sw_photo)
-sw_image.place(x = 250,y = 200,anchor="center")
+sw_image.place(x = 230,y = 200,anchor="center")
 
 #Start Button
-start_b = Button(win_root, text = "START"  ,font = " arial 10 bold", width = 30, height= 5, bd = 5 
-                  ,relief = GROOVE ,fg="red", bg= "white" , command =  loginWin)
-start_b.place(x = 250,y = 400,anchor='center')
+start_b = Button(win_root, text = "START",font = " arial 20 bold", width = 10, height= 2, bd = 5 
+                  ,fg="red" , command =  loginWin)
+start_b.place(x = 230,y = 380,anchor='center')
 
 
 #Swara Text
